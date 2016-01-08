c89794caeffafc465f3dd30e6a332ec2581775ec
Michael Williams <michael.williams@enspiral.com>
2016-01-08 18:50:45 +1300

    defer fetch till next tick
    this way we don't fetch during a render and annoy react

--- app/util/fetch-element.js
+++ app/util/fetch-element.js
@@ -3,10 +3,12 @@ import React from 'react'
 export default function fetchElement (store) {
   return function (Component, props) {
     if (Component.fetchData) {
-      Component.fetchData(
-        store.getState, store.dispatch,
-        props.location, props.params
-      )
+      process.nextTick(function () {
+        Component.fetchData(
+          store.getState, store.dispatch,
+          props.location, props.params
+        )
+      })
     }
     return React.createElement(Component, props)
   }
