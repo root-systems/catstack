746fc4a617b5f0a2da52acb01a26fa30f7bb790f
Michael Williams <michael.williams@enspiral.com>
2016-02-13 15:42:45 +1300

    upgrade feathers

--- app/client.js
+++ app/client.js
@@ -1,11 +1,12 @@
-import Client from 'feathers-client'
+import Client from 'feathers/client'
+import Rest from 'feathers-rest/client'
 import fetch from 'isomorphic-fetch'
 import Url from 'url'
 
 import config from 'app/config'
 
 const apiUrl = Url.format(config.api.url)
-const client = Client(apiUrl)
-  .configure(Client.fetch(fetch))
+const client = Client()
+  .configure(Rest(apiUrl).fetch(fetch))
 
 export default client
