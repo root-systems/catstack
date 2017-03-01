a3dd40c7280c74ff7f42fe47ded83e80c31b378f
Michael Williams <michael.williams@enspiral.com>
2017-03-01 18:14:44 +1300

    use bulk-require and bulkify correctly

--- modules/browser.js
+++ modules/browser.js
@@ -2,10 +2,5 @@ const bulk = require('bulk-require')
 const Path = require('path')
 
 module.exports = bulk(Path.join(__dirname, '..'), [
-  '+(app|client|config|log|pull|service)/**/!(browser|*.test).js'
-])
-// HACK remove circular bulk require artifacts
-delete module.exports.config.index
-delete module.exports.service.index
-delete module.exports.pull.index
-delete module.exports.log.index
+  '+(app|browser|client|config|log|pull|service)/**/!(browser|*.test).js'
+], { index: false })
--- modules/index.js
+++ modules/index.js
@@ -3,4 +3,4 @@ const Path = require('path')
 
 module.exports = bulk(Path.join(__dirname, '..'), [
   '+(app|cli|client|config|log|pull|service|server)/**/!(browser|*.test).js'
-])
+], { index: false })
