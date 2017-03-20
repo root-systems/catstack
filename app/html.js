b1f1d1ee4e7c8fff888aa533b5d7d0f4595074b6
Michael Williams <michael.williams@enspiral.com>
2017-03-20 17:55:22 +1300

    use hyps!

--- app/html.js
+++ app/html.js
@@ -1,12 +1,6 @@
-if (typeof window === 'undefined') {
-  window = require('global/window')
-  document = require('global/document')
-}
-
+const { keys } = Object
 const nest = require('depnest')
-const create = require('hyps/createElement')
-const morphdom = require('morphdom')
-const { default: H, cacheElementEventHandlers } = require('@skatejs/val')
+const { create, update } = require('hyps')
 const Hx = require('hyperx')
 
 module.exports = {
@@ -24,78 +18,26 @@ module.exports = {
     return nest('html', {
       create,
       update,
-      h: H(api.html.create),
-      hx: Hx(api.html.h)
+      h: api.html.create,
+      hx: Hx((tagName, properties, children) => {
+        return api.html.h(tagName, hSugarProperties(properties), children)
+      })
     })
   }
 }
 
-function update (fromNode, toNode, opts = {}) {
-  if (opts.events !== false) {
-    if (!opts.onBeforeElUpdated) opts.onBeforeElUpdated = copier
-  }
-
-  return morphdom(fromNode, toNode, opts)
-}
-
-// morphdom only copies attributes. we decided we also wanted to copy events
-// that can be set via attributes
-// 
-// inspiration from https://github.com/maxogden/yo-yo/blob/b4e1e8fe2e1081464c1fbdd1ad6c7a0ae7e24ad1/index.js
-function copier (fromNode, toNode) {
-  copyEvents(fromNode, toNode)
-  copyValues(fromNode, toNode)
-}
-
-function copyEvents (fromNode, toNode) {
-  const toEventHandlers = cacheElementEventHandlers.get(toNode)
-  const fromEventHandlers = cacheElementEventHandlers.get(fromNode)
-
-  // copy events:
-  uniqueKeys(toEventHandlers, fromEventHandlers).forEach(name => {
-    const toEventHandler = toEventHandlers[name]
-    const fromEventHandler = fromEventHandlers[name]
-    // if existing event handler does not match new event handler
-    if (fromEventHandler !== toEventHandler) {
-      // if existing event handler is defined
-      if (fromEventHandler !== undefined) {
-        // remove existing event handler
-        fromNode.removeEventListener(name, fromEventHandler)
-        delete fromEventHandlers[name]
-      }
-      // if new event handler is defined
-      if (toEventHandler !== undefined) {
-        // add new event handler
-        fromNode.addEventListener(name, toEventHandler)
-        fromEventHandlers[name] = toEventHandler
-      }
+function hSugarProperties (sugarProperties) {
+  var events = {}
+  var attributes = {}
+  var properties = { events, attributes }
+  keys(sugarProperties).forEach(key => {
+    const value = sugarProperties[key]
+    if (key.substring(0, 3) === 'ev-') {
+      events[key.substring(3)] = value
+    } else if (key.substring(0, 5) === 'attr-') {
+      attributes[key.substring(5)] = value
     }
+    properties[key] = value
   })
-}
-
-function copyValues (fromNode, toNode) {
-  var oldValue = fromNode.value
-  var newValue = toNode.value
-  // copy values for form elements
-  if ((
-    fromNode.nodeName === 'INPUT'
-    && fromNode.type !== 'file'
-  ) || fromNode.nodeName === 'SELECT') {
-    if (!newValue) {
-      toNode.value = fromNode.value
-    } else if (newValue !== oldValue) {
-      fromNode.value = newValue
-    }
-  } else if (fromNode.nodeName === 'TEXTAREA') {
-    if (toNode.getAttribute('value') === null) fromNode.value = toNode.value
-  }
-}
-
-// credit to paolo
-// https://github.com/0x00A/TIL/blob/master/javascript/unique-arrays.md
-function uniqueKeys (objA = {}, objB = {}) {
-  return Array.from(new Set([
-    ...Object.keys(objA),
-    ...Object.keys(objB)
-  ]))
+  return properties
 }
